e81aca635be5d9169daa35b95cfa226b7e54a8d3
daruan <daruan.rst@gmail.com>
2022-05-18 20:28:42 -0300

    1050

---
+++ src/com/beecrowd/Ex1050DDD/Main.java
@@ -0,0 +1,53 @@
+package com.beecrowd.Ex1050DDD;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * IMPORTANT:
+ * O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute
+ * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int ddd = SC.nextInt();
+        switch (ddd) {
+            case 61:
+                System.out.println("Brasilia");
+                break;
+            case 71:
+                System.out.println("Salvador");
+                break;
+            case 11:
+                System.out.println("Sao Paulo");
+                break;
+            case 21:
+                System.out.println("Rio de Janeiro");
+                break;
+            case 32:
+                System.out.println("Juiz de Fora");
+                break;
+            case 19:
+                System.out.println("Campinas");
+                break;
+            case 27:
+                System.out.println("Vitoria");
+                break;
+            case 31:
+                System.out.println("Belo Horizonte");
+                break;
+            default:
+                System.out.println("DDD nao cadastrado");
+                break;
+        }
+        SC.close();
+
+    }
+
+
+}
