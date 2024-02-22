7609c867e966f12e08cd03dea8557331456130ec
Daruan <daruan.rst@gmail.com>
2024-02-21 23:41:45 -0300

    2623

--- src/com/beecrowd/Ex2623CategoriesWithVariousProducts/Ex2623CategoriesWithVariousProducts.sql
+++ src/com/beecrowd/Ex2623CategoriesWithVariousProducts/Ex2623CategoriesWithVariousProducts.sql
@@ -1,6 +1,6 @@
-SELECT p.name, c.name FROM products p
+SELECT p.name as name, c.name as name FROM products p
 INNER JOIN categories c
-ON pr.id_categories = p.id
+ON p.id_categories = c.id
 WHERE c.id in (1,2,3,6,9)
 AND p.amount > 100
-ORDER BY c.id ASC
+ORDER BY c.id ASC;
